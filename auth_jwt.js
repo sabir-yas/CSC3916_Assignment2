c4e5c787783dd87f26ff6f093142043931a33f06
sabir-yas <cmneurolabanschutz@gmail.com>
2026-02-20 14:34:00 -0700

    initial commit

--- auth_jwt.js
+++ auth_jwt.js
@@ -3,18 +3,20 @@ var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 
 var opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-opts.secretOrKey = process.env.SECRET_KEY;
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.JWT_SECRET;
 
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    var user = db.find(jwt_payload.id);
+    // For assignment, we don't need a real DB
+    // Just accept the token payload
 
-    if (user) {
-        done(null, user);
+    if (jwt_payload) {
+        return done(null, jwt_payload);
     } else {
-        done(null, false);
+        return done(null, false);
     }
 }));
 
 exports.isAuthenticated = passport.authenticate('jwt', { session : false });
-exports.secret = opts.secretOrKey ;
+exports.secret = opts.secretOrKey;
